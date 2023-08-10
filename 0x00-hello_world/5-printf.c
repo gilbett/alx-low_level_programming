8af9ea4b6e7a48ca88008ff95f5595bf53d2f985
gilbett <bettkgilbert@outlook.com>
2023-08-10 09:36:51 -0700

    Print text with prinf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main -  This program prints a string using printf
+ * Return:  It should return 0 if it runs successfully
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
--- 0x00-hello_world/README.md
+++ 0x00-hello_world/README.md
@@ -3,3 +3,4 @@ Task 1. Compiler
 Task 2. Assembler
 Task 3. Name
 Task 4. Hello, puts
+Task 5. Hello, printf
