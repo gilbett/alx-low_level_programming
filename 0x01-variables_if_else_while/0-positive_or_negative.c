c25dc7bdeef7585398480e3b488faf167bd1a2a7
gilbett <bettkgilbert@outlook.com>
2023-08-11 07:55:34 -0700

    Task 0. Assign a random number

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /**
  * main - Entry point for the program
- * Return - returns zero if the program runs
+ * Return - returns zero if the program runs successfully
  */
 
 int main(void)
@@ -14,16 +14,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-	{
 		printf("%d is positive", n);
-	}
 	else if (n == 0)
-	{
 		printf("%d is zero", n);
-	}
 	else
-	{
 		printf("%d is negative", n);
-	}
 	return (0);
 }
