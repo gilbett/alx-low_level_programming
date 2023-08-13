193af4fe7d85db8710e68385020d7b587a9a9c56
gilbett <bettkgilbert@outlook.com>
2023-08-12 23:11:31 -0700

    Print all the numbers of base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Description: print all numbers of base 16 in lowercase
+ * Return: Always (0) Sucess
+ */
+
+int main(void)
+{
+	int i;
+	char character;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0')
+	}
+	for (character = 'a'; character <= 'f'; character++)
+	{
+		putchar(character);
+	}
+	putchar(character);
+	return (0);
+}
+
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -6,3 +6,5 @@ Task 4. Print alphabet with q and e missing
 Task 5. Numbers
 Task 6. Print digits using putchar
 Task 7. Prints the lowercase alphabet in reverse
+Task 8. Print numbers of base 16
+Task 8. Print numbers of base 16 in lowercase
