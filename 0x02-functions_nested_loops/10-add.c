a6a257183df66bd3fa1c27cc279560a3f6c91c77
gilbett <bettkgilbert@outlook.com>
2023-08-16 01:34:53 -0700

    Add two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - This function adds two integers
+ * @num1: The first number
+ * @num2: The second number
+ * Return: Always the result
+ */
+
+int add(int num1, int num2)
+{
+	int result = num1 + num2;
+
+	return (result);
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -4,3 +4,4 @@ Task 3. Check for lowercase character
 Task 4. Check for alphabetic character
 Task 7. Print the last digit of a number
 Task 9. Print the 9 times table
+Task 10. Add two integers
