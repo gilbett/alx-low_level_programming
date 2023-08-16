32b2129919c002a5db6403486efa19f4e45dd8c9
gilbett <bettkgilbert@outlook.com>
2023-08-16 00:32:36 -0700

    Print the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * times_table - Function that prints the 9 times table
+ */
+
+void times_table(void)
+{
+	int row;
+	int column;
+
+	for (row = 0; row <= 9; row++)
+	{
+		for (column = 0; column <= 9; column++)
+		{
+			int ans = row * column;
+
+			if (column != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar((ans / 10) + '0');
+				_putchar((ans % 10) + '0');
+			}
+			else
+			{
+				_putchar(ans + '0');
+			}
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -3,3 +3,4 @@ Task 2. Print 10 times the alphabet
 Task 3. Check for lowercase character
 Task 4. Check for alphabetic character
 Task 7. Print the last digit of a number
+Task 9. Print the 9 times table
