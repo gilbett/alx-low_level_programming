6fc0e799bc00a8bd2549f6f3f136004655b0d940
gilbett <bettkgilbert@outlook.com>
2023-08-17 10:03:30 -0700

    Check for a digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+/**
+ * _isdigit - This function checks for a digit
+ * @c: This is a character
+ * Return: Always (1) if is a digit, (0) otherwise
+ */
+
+int _isdigit(int c)
+{
+	if (c >= '0' && c <= '0')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,2 @@
 Task 0. Check for uppercase character
+Task 1. Function to check for a digit
