2c65da35575d41ad537d1da6391cbba9658942c1
gilbett <bettkgilbert@outlook.com>
2023-08-18 02:40:34 -0700

    Print a square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_square - This function prints a square
+ * @size: size of the square
+ */
+
+void print_square(int size)
+{
+	int m;
+	int n;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	for (m = 0; m < size; m++)
+	{
+		for (n = 0; n < size; n++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}     
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -5,3 +5,4 @@ Task 4. Prints the numbers from 0 to 9
 Task 5. Print numbers 10 times
 Task 6. Function that draws a straight line
 Task 7. Function to draw diagonal line
+Task 8. Function to print square
